d8ffae7c62f529eaabfd99901eaa0d4a109d8ee9
zhangliu463884153 <38039217+zhangliu463884153@users.noreply.github.com>
2019-10-02 19:01:21 +0800

    update

---
+++ Sbox常用性质.cpp
@@ -0,0 +1,113 @@
+//
+//  main.cpp
+//  testsbox
+
+#include <stdio.h>
+#include <memory.h>
+#include <time.h>
+#include <math.h>
+
+typedef unsigned int word32;
+
+
+void DiffSpectrum(word32* s, word32 N,char *outputfile)
+{
+
+    word32 ds[N+1] ;
+    word32 dt[N];
+    word32 ecx = 0;
+
+    memset(ds, 0, sizeof(word32)*(N+1));
+    time_t start, end;
+    double usted;
+
+    start = clock();
+
+    word32 x, d;
+    for (d = 1; d < N; d ++) {
+        memset(dt, 0, sizeof(word32)*N);
+        for (x = 0; x < N; x ++) {
+            dt[s[x]^s[x^d]] ++;
+        }
+
+        for (x = 0; x < N; x ++) {
+            ds[dt[x]] ++;
+        }
+    }
+
+    end = clock();
+    
+    FILE *fp = fopen(outputfile, "a+");
+    usted = double(end - start) / CLOCKS_PER_SEC;
+    fprintf(fp,"used time = %lf\n", usted);
+
+    for (d = 0; d <= N; d ++) {
+        if (ds[d] > 0) {
+            fprintf(fp,"%u, %u\n", d, ds[d]);
+            ecx ++;
+        }
+    }
+    fprintf(fp,"ecx = %u\n", ecx);
+}
+
+//GetN与ReadSbox代码有重复，GetN只是为了获取文件行数，不将文件内容存储。 
+int GetN( char* sboxfile) 
+{
+    int count = 0;
+    int temp;//存储临时获取的文件内容 
+    FILE* fp = fopen(sboxfile, "r");
+    if (fp == NULL) {
+        return 0;
+    }
+
+    while (fscanf(fp, "%d\n", &temp) != EOF) {
+        count ++; 
+    }
+
+    fclose(fp);
+    
+    return count;
+}
+void ReadSbox(char* sboxfile, word32* sbox)
+{
+    int count = 0;
+    FILE* fp = fopen(sboxfile, "r");
+    if (fp == NULL) {
+        printf("文件内容为空\n"); 
+    }
+
+    while (fscanf(fp, "%d\n", &sbox[count]) != EOF) {
+        count ++;
+    }
+    fclose(fp);
+
+}
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    
+//
+//    word32 x;
+//    for (x = 0; x < N; x ++) {
+//        s[x] = x;
+//    }
+    printf("请输入Sbox文件名(需文件格式后缀名,例如：sbox.txt):\n");
+	char inputfile[100];
+	gets(inputfile);
+
+	
+	printf("请输入结果输出文件名(需文件格式后缀名):\n");
+	char outputfile[100];
+	gets(outputfile);
+
+	
+    word32 num = GetN(inputfile);
+    word32 N=ceil(log10(num)/log10(2));
+    printf("N= %d\n", N);
+    word32 s[num];
+	ReadSbox(inputfile, s);
+    
+    DiffSpectrum(s,num,outputfile);
+    return 0;
+    
+} 
